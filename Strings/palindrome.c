6545b17175a30cdbc13dc4fb031caa271ec4cc4a
salmanfarooq29 <41887969+salmanfarooq29@users.noreply.github.com>
2018-08-25 14:55:19 +0530

    Update palindrome.c

--- Strings/palindrome.c
+++ Strings/palindrome.c
@@ -1,65 +1,32 @@
-#include<stdio.h>
-
-void create(int []);
-void down_adjust(int [],int);
-
-void main()
+#include <stdio.h>
+#include <string.h>
+int main()
 {
-    int heap[30],n,i,last,temp;
-    printf("Enter no. of elements:");
-    scanf("%d",&n);
-    printf("\nEnter elements:");
-    for(i=1;i<=n;i++)
-        scanf("%d",&heap[i]);
-    
-    //create a heap
-    heap[0]=n;
-    create(heap);
-    
-    //sorting
-    while(heap[0] > 1)
-    {
-        //swap heap[1] and heap[last]
-        last=heap[0];
-        temp=heap[1];
-        heap[1]=heap[last];
-        heap[last]=temp;
-        heap[0]--;
-        down_adjust(heap,1);
-    }
-    
-    //print sorted data
-    printf("\nArray after sorting:\n");
-    for(i=1;i<=n;i++)
-        printf("%d ",heap[i]);
-}
-
-void create(int heap[])
-{
-    int i,n;
-    n=heap[0]; //no. of elements
-    for(i=n/2;i>=1;i--)
-        down_adjust(heap,i);
-}
-
-void down_adjust(int heap[],int i)
-{
-    int j,temp,n,flag=1;
-    n=heap[0];
-    
-    while(2*i<=n && flag==1)
-    {
-        j=2*i;    //j points to left child
-        if(j+1<=n && heap[j+1] > heap[j])
-            j=j+1;
-        if(heap[i] > heap[j])
-            flag=0;
-        else
-        {
-            temp=heap[i];
-            heap[i]=heap[j];
-            heap[j]=temp;
-            i=j;
-        }
-    }
+   char str[], i=0;
+   int str_len;
+ 
+   printf("Enter a word to check for Palindrome : ");
+   scanf("%s",str);
+   printf("\nEntered word is : %s", str);
+ 
+   while(1)
+   {
+      str_len = strlen(str) - (i+1);
+ 
+      if (str[i] == str[str_len])
+      {
+         if (i == str_len || i+1 == str_len)
+         {
+            printf("\nThe entered word \"%s\" is a palindrome", str);
+            break;
+         }
+         i = i+1;
+      }
+      else
+      {
+         printf("\nThe entered word \"%s\" is not a palindrome", str);
+         break;
+      }
+   }
+   return 0;
 }
