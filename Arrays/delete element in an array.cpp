2a6fc48b1b33f25274d06d436efe97b636e4f88d
salmanfarooq29 <41887969+salmanfarooq29@users.noreply.github.com>
2018-08-25 14:19:00 +0530

    Add files via upload

---
+++ Arrays/delete element in an array.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int array[100], position, i, n;
+    
+    printf("Enter the size of the array\n");
+    scanf("%d", &n);
+    
+    printf("Enter elements of the array\n", n);
+    
+    for (i = 0; i < n; i++)
+        scanf("%d", &array[i]);
+    
+    printf("Enter the location where you wish to delete element\n");
+    scanf("%d", &position);
+    
+    if (position >= n+1)
+        printf("Deletion not possible.\n");
+    else
+    {
+        for (i = position - 1; i < n - 1; i++)
+            array[i] = array[i+1];
+        
+        printf("Resultant array:\n");
+        
+        for (i = 0; i < n - 1; i++)
+            printf("%d\n", array[i]);
+    }
+    
+    return 0;
+}
